b898d7cd3086fc1e5722c6f1a6cfdaafb5b1bae0
amberm <nekosenpai@gmail.com>
2019-01-25 11:11:09 -0700

    styled components drool, CSS RULZ!

--- lambdatimes/src/components/Header.js
+++ lambdatimes/src/components/Header.js
@@ -1,15 +1,48 @@
 import React from 'react';
 
+import styled from 'styled-components'
+
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
 
+const Headdy = styled.div `
+display: flex;
+justify-content: space-between;
+align-items: center;
+flex-direction: row;
+background-color: #fff;
+border-bottom: 1px solid lightgrey;
+width: 100%;
+height: 120px;
+margin-top: 44px;
+padding-bottom: 15px;
+`;
+
+const JediMindTrick = styled.h1 `
+font-size: 60px;
+  font-family: Didot, serif;
+  font-weight: bold;
+  flex: 8;
+  text-align: center;
+  color: #000;
+`;
+
+const StarTrekSucks = styled.span `
+align-self: flex-end;
+font-size: 11px;
+font-weight: bold;
+letter-spacing: 1px;
+padding: 10px;
+`;
+
+
 const Header = () => {
   return (
-    <div className="header">
-      <span className="date">SMARCH 32, 2018</span>
-      <h1>Lambda Times</h1>
-      <span className="temp">98°</span>
-    </div>
+    <Headdy>
+      <StarTrekSucks>SMARCH 32, 2018</StarTrekSucks>
+      <JediMindTrick>Lambda Times</JediMindTrick>
+      <StarTrekSucks>98°</StarTrekSucks>
+    </Headdy>
   )
 }
 
--- lambdatimes/src/components/TopBar.js
+++ lambdatimes/src/components/TopBar.js
@@ -1,23 +1,98 @@
 import React from 'react';
 
+import styled from 'styled-components'
+
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
 
+const TopBar1 = styled.div `
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: none;
+  flex-direction: row;
+  position: fixed;
+  height: 44px;
+  background-color: #333;
+`;
+
+const ContCent = styled.div `
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  flex: 3;
+  font-size: 9px;
+`;
+
+const SpanCent = styled.div `
+  cursor: pointer;
+  margin-right: 5%;
+`;
+
+const ContLeft = styled.div `
+display: flex;
+justify-content: none;
+align-items: center;
+flex-direction: row;
+flex: 1;
+font-size: 11px;
+`;
+
+const ContRight = styled.div `
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  flex-direction: row;
+  flex: 1;
+  font-size: 11px;
+  font-weight: bold;
+`;
+
+const ContMain = styled.div `
+width: 100%;
+display: flex;
+justify-content: none;
+align-items: none;
+flex-direction: row;
+color: #fff;
+letter-spacing: 1px;
+padding: 0 10px;
+`;
+
+const TippyTop = styled.div `
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: none;
+  flex-direction: row;
+  position: fixed;
+  height: 44px;
+  background-color: #333;
+`;
+
+
 const TopBar = () => {
   return (
-    <div className="top-bar">
-      <div className="container">
-        <div className="container-left">
-          <span>TOPICS</span><span>SEARCH</span>
-        </div>
-        <div className="container-center">
-          <span>GENERAL</span><span>BROWNBAG</span><span>RANDOM</span><span>MUSIC</span><span>ANNOUNCEMENTS</span>
-        </div>
-        <div className="container-right">
-          <span>LOG IN</span>
-        </div>
-      </div>
-    </div>
+    <TippyTop>
+
+      <ContMain>
+
+        <ContLeft>
+          <SpanCent>TOPICS</SpanCent><SpanCent>SEARCH</SpanCent>
+        </ContLeft>
+
+        <ContCent>
+          <SpanCent>GENERAL</SpanCent><SpanCent>BROWNBAG</SpanCent><SpanCent>RANDOM</SpanCent><SpanCent>MUSIC</SpanCent><SpanCent>ANNOUNCEMENTS</SpanCent>
+        </ContCent>
+
+        <ContRight>
+          <SpanCent>LOG IN</SpanCent>
+        </ContRight>
+
+      </ContMain>
+
+    </TippyTop>
   )
 }
 
